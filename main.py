9d7b6e3fe2f00eac29a464f02976c9950688cc3a
RAMGIRI  DEEPAK KUMAR <142658497+DEEPAK-RAMGIRI@users.noreply.github.com>
2025-08-19 22:20:55 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -5,9 +5,27 @@ import spotipy
 import os
 from dotenv import load_dotenv
 
+
 load_dotenv()
 
-
+st.markdown(
+    """
+    <style>
+    .stApp {
+        background: linear-gradient(to top, #30cfd0 0%, #330867 100%) !important;
+        background-attachment: fixed;
+        color: white; /* optional: make text readable */
+    }
+    .block-container {
+        padding-top: 2rem;
+        padding-bottom: 2rem;
+        padding-left: 3rem;
+        padding-right: 3rem;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
 SPOTIPY_CLIENT_ID=os.getenv("CLIENT_ID")
 SPOTIPY_CLIENT_SECRET=os.getenv('CLIENT_SECRET')
@@ -42,7 +60,7 @@ def reccommend_song(song):
   posters = []
   
   
-  for i in recom_distance[1:6]:
+  for i in recom_distance[1:5]:
     artist = data.iloc[i[0]].artist
     songs.append(data.iloc[i[0]].song)
     posters.append(get_cover_photo(data.iloc[i[0]].song,artist))
@@ -57,7 +75,7 @@ selected_song = st.selectbox("Type or select the eng song only",songlist)
 
 if st.button("Show Reccomendations "):
     songs, posters = reccommend_song(selected_song)
-    cols = st.columns(5) 
+    cols = st.columns(4) 
     for i, col in enumerate(cols):
         with col:
             st.text(songs[i])
